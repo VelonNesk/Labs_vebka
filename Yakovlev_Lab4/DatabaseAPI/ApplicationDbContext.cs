2e19901c6d8e14a2408f7d983976e3085f44d6e3
Виктор <104264383+VelonNesk@users.noreply.github.com>
2023-06-03 02:12:55 +0300

    Add files via upload

---
+++ Yakovlev_Lab4/DatabaseAPI/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DatabaseAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAPI
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<DBUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+
+            modelBuilder.Entity<DBUser>(dbUser =>
+            {
+                dbUser.ToTable("users");
+                dbUser.HasKey(user => user.Id);
+                dbUser.Property(user => user.Id).HasColumnName("id");
+                dbUser.Property(user => user.Login).HasColumnName("login");
+                dbUser.Property(user => user.Password).HasColumnName("password");
+                dbUser.Property(user => user.Name).HasColumnName("name");
+            });
+        }
+    }
+}
