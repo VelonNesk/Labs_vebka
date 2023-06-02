1ee8e41b9d3b69f62db4a8a606fbf9a183ea334b
Виктор <104264383+VelonNesk@users.noreply.github.com>
2023-06-03 02:08:06 +0300

    Add files via upload

---
+++ Yakovlev_Lab1/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TspuWebLabs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] int count)
+        {
+            Random random = new Random();
+            IEnumerable<int> values = Enumerable.Range(1, count).Select(x => random.Next(Int32.MinValue, Int32.MaxValue));
+
+            return Ok(values);
+        }
+    }
+}
